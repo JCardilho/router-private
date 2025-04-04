c813a72e78c6d6d8df96c79280818b6a957f7cd3
JCardilho <jose.1433@hotmail.com>
2025-04-04 15:08:27 -0300

    feat correção do token

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,15 @@
+
+
+export default function PublicLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+        <h1>Public</h1>
+        {children}
+
+    </div>
+  );
+}
